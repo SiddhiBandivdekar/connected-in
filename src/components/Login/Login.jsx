0b8e5e70e045ea48294dcb9d4e068a9d01fdb45d
Siddhi Bandivdekar <bandivdekarsiddhi@gmail.com>
2023-02-03 15:27:25 +0530

    Login page updated with styling

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,7 +2,32 @@ import React from "react";
 import "./Login.scss";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="container">
+      <nav>
+        <a href="/">
+          <img src="/images/loginLogo.png" alt="" />
+        </a>
+        <div className="right-nav">
+          <div className="join">Join now!</div>
+          <div className="signIn">Sign in</div>
+        </div>
+      </nav>
+      <section>
+        <div>
+          <h1>Welcome to your professional community</h1>
+          <img className="login-logo-image" src="/images/meeting.png" alt="" />
+        </div>
+
+        <div className="form">
+          <button className="google">
+            <img className="google-img" src="/images/google.png" alt="" />
+            Sign in with Google
+          </button>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Login;
