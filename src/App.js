e6e8f1775f65af235e1a05ef2e00f9bac5ba0bdf
Siddhi Bandivdekar <bandivdekarsiddhi@gmail.com>
2023-02-03 15:26:47 +0530

    Home route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import "./App.css";
 import Login from "./components/Login/Login";
-// import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+
+const HomePage = () => (
+  <>
+    <Header />
+    <Home />
+  </>
+);
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route
+          path="/home"
+          // render={() => (
+          //   <>
+          //     <Header />
+          //     <Home />
+          //   </>
+          // )}
+
+          element={<HomePage />}
+        />
       </Routes>
     </div>
   );
