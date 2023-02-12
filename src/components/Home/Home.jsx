732ee543c620a7b71e75c26cdb30bcfe076dd8cc
Siddhi Bandivdekar <bandivdekarsiddhi@gmail.com>
2023-02-12 15:59:34 +0530

    Main body of home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./Home.scss";
 
-const Home = () => {
-  return <div>Home</div>;
+const Home = (props) => {
+  return (
+    <div className="container">
+      <div className="content">
+        <div className="layout">
+          <div className="left">Left side</div>
+          <div className="main">Main</div>
+          <div className="right">Right side</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
