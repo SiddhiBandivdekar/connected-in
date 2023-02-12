c5f10fe1cd9d3507f8dc0995e9c1b1450b7c0208
Siddhi Bandivdekar <bandivdekarsiddhi@gmail.com>
2023-02-12 16:06:30 +0530

    Updated Home with the Left Right and Main component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import "./Home.scss";
+import LeftSide from "./LeftSide/LeftSide";
+import Main from "./Main/Main";
+import RightSide from "./RightSide/RightSide";
 
 const Home = (props) => {
   return (
     <div className="container">
       <div className="content">
         <div className="layout">
-          <div className="left">Left side</div>
-          <div className="main">Main</div>
-          <div className="right">Right side</div>
+          <div className="left">
+            <LeftSide />
+          </div>
+          <div className="main">
+            <Main />
+          </div>
+          <div className="right">
+            <RightSide />
+          </div>
         </div>
       </div>
     </div>
