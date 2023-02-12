d4c0e0c5b0ff2da38bd924964e0dc8554bf67eab
Siddhi Bandivdekar <bandivdekarsiddhi@gmail.com>
2023-02-12 16:05:35 +0530

    Created the components for left, right and main

---
+++ src/components/Home/LeftSide/LeftSide.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./LeftSide.scss";
+
+const LeftSide = () => {
+  return (
+    <div className="container">
+      <div className="card">LeftSide</div>
+    </div>
+  );
+};
+
+export default LeftSide;
