2ceb5b35cbea8a677834b537ef3a26a788a7fbc8
Siddhi Bandivdekar <bandivdekarsiddhi@gmail.com>
2023-02-13 17:55:13 +0530

    Updated code

--- src/components/Home/Main/Main.jsx
+++ src/components/Home/Main/Main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Main.scss";
 
-const Main = () => {
+const Main = (props) => {
   return <div>Main</div>;
 };
 
--- src/components/Home/RightSide/RightSide.jsx
+++ src/components/Home/RightSide/RightSide.jsx
@@ -1,8 +1,55 @@
 import React from "react";
 import "./RightSide.scss";
+import InfoIcon from "@mui/icons-material/Info";
+import TagRoundedIcon from "@mui/icons-material/TagRounded";
+import ArrowRightAltRoundedIcon from "@mui/icons-material/ArrowRightAltRounded";
 
 const RightSide = () => {
-  return <div>RightSide</div>;
+  return (
+    <div className="main">
+      <div className="followCard">
+        <div className="title">
+          <h2>Add to your feed</h2>
+          <InfoIcon />
+        </div>
+
+        <div className="feedList">
+          <li>
+            <a>
+              <div>
+                <TagRoundedIcon className="avatar" />
+              </div>
+            </a>
+            <div>
+              <span>#Connectedin</span>
+              <button>Follow</button>
+            </div>
+          </li>
+          <li>
+            <a>
+              <div>
+                <TagRoundedIcon className="avatar" />
+              </div>
+            </a>
+            <div>
+              <span>#Video</span>
+              <button>Follow</button>
+            </div>
+          </li>
+        </div>
+        <div className="recommendation">
+          View all recommendations
+          <ArrowRightAltRoundedIcon />
+        </div>
+      </div>
+      <div className="bannerCard">
+        <img
+          src="https://static-exp1.licdn.com/scds/common/u/images/promo/ads/li_evergreen_jobs_ad_300x250_v1.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default RightSide;
